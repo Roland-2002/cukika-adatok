a963d3eea9ad20658a26902db10d838367abe563
Roland-2002 <mihokbalazsm@gmail.com>
2026-08-18 16:51:25 +0200

    Update feladat.js

--- feladat.js
+++ feladat.js
@@ -1,18 +1,3 @@
-// Megkeressük a gombokat az ID-juk alapján, és beállítjuk a működésüket
-
-document.getElementById("gomb-1").onclick = () => {
-    alert("Helyes!");
-};
-
-document.getElementById("gomb-2").onclick = () => {
-    alert("Sajnos nem.");
-};
-
-document.getElementById("gomb-3").onclick = () => {
-    alert("Sajnos nem.");
-};
-
-
 // Megkeressük az összes válaszlehetőség gombot a letöltött HTML-ben
 document.querySelectorAll('.valasz-gomb').forEach(gomb => {
     gomb.addEventListener('click', function() {
