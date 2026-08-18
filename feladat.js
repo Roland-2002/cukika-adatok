c5aac2c1daf0b12b1df90fc3939667df6aed448c
Roland-2002 <mihokbalazsm@gmail.com>
2026-08-18 16:51:07 +0200

    Update feladat.js

--- feladat.js
+++ feladat.js
@@ -11,3 +11,17 @@ document.getElementById("gomb-2").onclick = () => {
 document.getElementById("gomb-3").onclick = () => {
     alert("Sajnos nem.");
 };
+
+
+// Megkeressük az összes válaszlehetőség gombot a letöltött HTML-ben
+document.querySelectorAll('.valasz-gomb').forEach(gomb => {
+    gomb.addEventListener('click', function() {
+        // Vizuális visszajelzés: levesszük a jelölést a többiről, és erre rátesszük
+        document.querySelectorAll('.valasz-gomb').forEach(g => g.classList.remove('aktiv'));
+        this.classList.add('aktiv');
+        
+        // ELMENTJÜK a gomb szövegét a telefon rejtett memóriájába
+        localStorage.setItem('aktualisValasz', this.innerText); 
+        console.log('Ideiglenesen elmentve a memóriába:', this.innerText);
+    });
+});
