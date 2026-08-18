a3cbf32dc0a12c4e755bf28a1fe1c736a94089c0
Roland-2002 <mihokbalazsm@gmail.com>
2026-08-18 14:23:14 +0200

    Add files via upload

---
+++ feladat.js
@@ -0,0 +1,13 @@
+// Megkeressük a gombokat az ID-juk alapján, és beállítjuk a működésüket
+
+document.getElementById("gomb-1").onclick = () => {
+    alert("Helyes!");
+};
+
+document.getElementById("gomb-2").onclick = () => {
+    alert("Sajnos nem.");
+};
+
+document.getElementById("gomb-3").onclick = () => {
+    alert("Sajnos nem.");
+};
